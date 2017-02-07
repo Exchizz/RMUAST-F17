9183bcbec6cf883c2b6af10dc5bd7dc401fb11b1
Mathias <manee12@student.sdu.dk>
2017-02-07 10:20:49 +0100

    Created 3 subplots instead of 3 individual plots

--- Lection1/nmea/nmea.py
+++ Lection1/nmea/nmea.py
@@ -13,59 +13,65 @@ class NMEA:
 		return array
 
 nmea = NMEA()
-#file = nmea.parse_nmea("nmea_trimble_gnss_eduquad_flight.txt")
-file = nmea.parse_nmea("nmea_ublox_neo_24h_static_gpgga.txt")
+filename="nmea_trimble_gnss_eduquad_flight.txt"
+filename="nmea_ublox_neo_24h_static_gpgga.txt"
+file = nmea.parse_nmea(filename)
 
 
 kml_class = kmlclass()
 
 i = 0
-x_sat = []
+x_obs = []
 y_sat = []
 y_msl = []
 y_hdop = []
+
 kml_class.begin("test.kml", "somename", "some more text", 3.0)
 for msg in file:
-	if msg[0] == "":
-		continue
 	try:
-		float(msg[4])
-		print msg[2], ", ", msg[4], ", ", msg[9], ", ", len(msg)
+		# Convert from ddmm.mmmm  (degree minutes) to  dd (decimal degree)
+		lat = int(float(msg[2])/100)
+		degDec=(float(msg[2])-lat*100)/60;
+		lat = lat + degDec;
+
+		lon = int(float(msg[4])/100)
+		degDec=(float(msg[4])-lon*100)/60;
+		lon = lon + degDec;
 	except:
 		continue
 
 
 
-	lat = int(float(msg[2])/100)
-	degDec=(float(msg[2])-lat*100)/60;
-	lat = lat + degDec;
-
-
-
-	lon = int(float(msg[4])/100)
-	degDec=(float(msg[4])-lon*100)/60;
-	lon = lon + degDec;
 
 	kml_class.trkpt( lat, lon, int(float(msg[9]))) # lon, lat
 
 	y_sat.append(int(float(msg[7])))
 	y_msl.append(int(float(msg[9])))
-	x_sat.append(i)
+	x_obs.append(i)
 
 	y_hdop.append(int(float(msg[8])))
 	i+=1
 kml_class.end()
 
-# Create plt with HDOP
-plt.scatter(x_sat, y_hdop)
-plt.show()
+
+f, axarr = plt.subplots(3,  sharex=True	)
+axarr[0].scatter(x_obs, y_hdop)
+axarr[0].set_title('HDOP')
+axarr[0].set_xlabel("Observations")
+axarr[0].set_ylabel("Horizontal accuracy")
+
+axarr[1].scatter(x_obs,y_sat)
+axarr[1].set_title('Number of sattelites')
+axarr[1].set_xlabel("Observations")
+axarr[1].set_ylabel("# of sattelites")
+
+axarr[2].scatter(x_obs, y_msl)
+axarr[2].set_title('Height (MSL)')
+axarr[2].set_xlabel("Observations")
+axarr[2].set_ylabel("Height [m]")
 
 
-# Create plt with MSL
-plt.scatter(x_sat, y_msl)
-plt.show()
-
-
-# Create one plt of sattelittes
-plt.scatter(x_sat, y_sat)
+f.subplots_adjust(hspace=0.3)
+plt.xlim(0,len(x_obs)-1)
+plt.suptitle(filename, fontsize=20)
 plt.show()
