f0e8f820d660c5abfb443140fbb239fe6fef9df3
Mathias Neerup <manee12@student.sdu.dk>
2017-03-22 18:01:05 +0100

    Works with bang-bang controller

--- mavros_lecture_ws/src/off_board/src/off_board_node.cpp
+++ mavros_lecture_ws/src/off_board/src/off_board_node.cpp
@@ -7,15 +7,11 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
-#include "opencv2/opencv.hpp"
 #include "../headers/image_converter.hpp"
-
 #define COLOR       0
 
 using namespace std;
 using namespace cv;
-
-
 
 mavros_msgs::State current_state;
 geometry_msgs::PoseStamped localposemsg;
@@ -26,6 +22,7 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg){
 void localpose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
   localposemsg = *msg;
 }
+
 
 void onmouse(int event, int x, int y, int flags, void* param){
     cv::Mat _img = *((cv::Mat*)(param));
@@ -38,7 +35,6 @@ void onmouse(int event, int x, int y, int flags, void* param){
             int(intensity.val[1]) << ", " << int(intensity.val[0]) << ")" << std::endl;
     }
 }
-
 bool find_center(Mat &image, Point2f &result){
     Mat segmented;
 
@@ -75,37 +71,36 @@ bool find_center(Mat &image, Point2f &result){
     return true;
 }
 
-int main(int argc, char **argv){
-
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
-    ros::Subscriber localpose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 1, localpose_cb);
-
-
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
-
+    ros::Subscriber localpose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 1, localpose_cb);
     ImageConverter image_conv("/iris/camera/image_raw", COLOR);
-
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
+    //namedWindow("Image", 1);
+    //setMouseCallback("Image", onmouse, NULL);
     // wait for FCU connection
     while(ros::ok() && current_state.connected){
         ros::spinOnce();
         rate.sleep();
     }
 
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
 
     //send a few setpoints before starting, or else you won't be able to switch to offboard mode
     for(int i = 100; ros::ok() && i > 0; --i){
-        geometry_msgs::PoseStamped pose;
-        pose.pose.position.x = 0; // Meters
-        pose.pose.position.y = 0; // Meters
-        pose.pose.position.z = 2; // Meters
+        std::cout << "Starting " << 100-i << "%\r" << std::endl;
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
@@ -119,69 +114,71 @@ int main(int argc, char **argv){
 
     ros::Time last_request = ros::Time::now();
 
-
     while(ros::ok()){
         if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.success){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.success){
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
         } else {
             if( !current_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) && arm_cmd.response.success){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
             }
         }
-
         cv::Mat image = image_conv.get_image();
 
-        std::cout << __LINE__ << "I'm here " << std::endl;
         if(!image.empty()){
-            Point2f center;
-            if(find_center(image, center)){
-                circle(image, center, 10, Scalar(255,255,255));
-                cv::imshow("Image", image);
-                waitKey(1);
-                int camx = center.x; // px
-                int camy = center.y; // px
-                int d = 2; // meters
-                float f = 0.01;
+          Point2f center;
+          if(find_center(image, center)){
+              int camx = center.x; // px
+              int camy = center.y; // px
 
-                int imgwidth = image.cols;
-                int imgheight = image.rows;
+              int imgwidth = image.cols;
+              int imgheight = image.rows;
+              circle(image, center, 10, Scalar(255,255,255));
+              circle(image, Point2f(imgwidth/2, imgheight/2), 40, Scalar(255,0,255));
 
-            // topic /mavros/local_position/pose
-              // geometry_msgs/PoseStamped
-                float X = (camx*d)/f;
-                float Y = (camy*d)/f;
 
-                X = localposemsg.pose.position.x + X - imgwidth/2;
-                Y = localposemsg.pose.position.y + Y - imgheight/2;
+              int d = 2; // meters
 
-                bool Markerfound = true;
 
-                geometry_msgs::PoseStamped pose;
+              float Y = localposemsg.pose.position.y;
+              float X = localposemsg.pose.position.x;
 
-                if(Markerfound){
-                  pose.pose.position.x = X; // Meters
-                  pose.pose.position.y = Y; // Meters
-                  pose.pose.position.z = d; // Meters
-                } else {
-                  pose.pose.position.x = 0; // Meters
-                  pose.pose.position.y = 0; // Meters
-                  pose.pose.position.z = d; // Meters
-                }
+              std::cout << "image width: " << imgwidth << std::endl;
+              std::cout << "relative posy: " << camy-imgheight/2 << " posx: " << camx-imgwidth/2 << std::endl;
+              
+              if((camy-imgheight/2) > 0){
+                std::cout << "Go back" << std::endl;
+                X-=0.1;
+              } else {
+                std::cout << "Go forward" << std::endl;
+                X+=0.1;
+              }
 
-                local_pos_pub.publish(pose);
+              if(camx-imgwidth/2 > 0){
+                Y-=0.1;
+              } else {
+                Y+=0.1;
+              }
 
+              std::cout << "X: " << X << " Y: " << Y << std::endl;
+
+              pose.pose.position.x = X; // Meters
+              pose.pose.position.y = Y; // Meters
+              pose.pose.position.z = d; // Meters
             }
-
-        } else
-            cout << "Image empty" << endl;
+            cv::imshow("Image", image);
+            waitKey(1);
+        }
+        local_pos_pub.publish(pose);
 
         ros::spinOnce();
         rate.sleep();
