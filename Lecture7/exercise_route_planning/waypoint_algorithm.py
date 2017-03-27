155500896d551dad8ab0ad8174ba7e9e05fcfa45
Mathias Neerup <manee12@student.sdu.dk>
2017-03-27 16:00:39 +0200

    Waypoint algorithm also exports to qground compatible format, your welcome :b

--- Lecture7/exercise_route_planning/waypoint_algorithm.py
+++ Lecture7/exercise_route_planning/waypoint_algorithm.py
@@ -1,15 +1,25 @@
 # Waypoint algorithm
 import math
 
+from export_kml import kmlclass
+
+from utm import utmconv
+from math import fabs
+
+utm = utmconv();
+
+
+# Plot dat' shit
+kml_class = kmlclass()
+
 # Open file
 f = open("aqlogreader/coordinates_in_utm.txt", "r")
 
 # Read Data
-
 data_waypoints = []
 for line in f:
-    (easting, northing) = line.split(",");
-    data_waypoints.append((float(easting), float(northing)))
+    (easting, northing, height) = line.split(",");
+    data_waypoints.append((float(easting), float(northing), float(height)))
 
 new_waypoints = []
 new_waypoints.append(0)
@@ -43,7 +53,53 @@ while(len(new_waypoints) < max_points and not stop_condition):
             new_waypoints.append(furthest_away_waypoint[1])
             stop_condition = False
 
-print new_waypoints
+print "Number of waypoints found: ", len(new_waypoints)
+kml_class.begin("Waypoints.kml", "Waypoints", "Waypoints", 3)
+zone = 32
+hemisphere = "N"
+kml_class.trksegbegin("test","test2", "blue", "relativeToGround")
+wpts = []
+for index in new_waypoints:
+	(lat, lon) = utm.utm_to_geodetic(hemisphere, zone, float(data_waypoints[index][0]), float(data_waypoints[index][1]))
+
+	kml_class.trkpt(lat, lon, float(data_waypoints[index][2]))
+	wpts.append( (lat, lon, float(data_waypoints[index][2])) )
+kml_class.trksegend()
+kml_class.end()
 
 
-#for i in range(32):
+
+# qgc parameters
+qgc_dunno = 0
+qgc_frame_takeoff = 0 # 0 = abs 3 = relative
+qgc_frame_wpt = 0 # 0 = abs 3 = relative
+qgc_poi_heading = 3.14
+qgc_vert_vmax = 1.0
+qgc_radius = 3.5
+qgc_loiter = 1
+qgc_hori_vmax = 10
+qgc_yaw = 3.14
+qgc_dunno2 = 1
+
+
+
+# qgc constants
+MAV_CMD_NAV_WAYPOINT = 16
+MAV_CMD_NAV_TAKEOFF = 22
+
+
+
+
+# export to QGC waypoint list
+f = open ('waypoints.txt', 'w')
+f.write ('QGC WPL 120\n')
+lat = wpts[0][0]
+lon = wpts[0][1]
+alt = wpts[0][2]
+
+f.write ('%d\t%d\t%d\t%d\t%.2f\t%.0f\t%.2f\t%.2f\t%.8f\t%.8f\t%.3f\t%d\n' % (0, qgc_dunno, qgc_frame_takeoff, MAV_CMD_NAV_TAKEOFF, qgc_radius, qgc_loiter*1000, qgc_poi_heading, qgc_vert_vmax, lat, lon, alt, qgc_dunno2))
+
+for wpt in wpts:
+        f.write ('%d\t%d\t%d\t%d\t%.2f\t%.0f\t%.2f\t%.2f\t%.8f\t%.8f\t%.3f\t%d\n' % (i+1, qgc_dunno, qgc_frame_wpt, MAV_CMD_NAV_WAYPOINT, qgc_radius, qgc_loiter*1000, qgc_hori_vmax, qgc_yaw, wpt[0], wpt[1], wpt[2], qgc_dunno2))
+f.close
+
