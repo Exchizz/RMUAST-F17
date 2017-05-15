f5e370a0270bec5133b0b565eaa5a2c4576f6e72
Vincent Klyverts Tofterup <vincenttofterup@gmail.com>
2017-05-15 16:19:23 +0200

    just fixing Christians stuff

--- FinalProject/python_telemetry/startup_telemetry.sh
+++ FinalProject/python_telemetry/startup_telemetry.sh
@@ -2,4 +2,4 @@
 
 sudo chmod 666 /dev/ttyUSB0
 stty -F /dev/ttyUSB0 57600
-while true; do echo "$GTPOS,12931,Drone,1,2,3" > /dev/ttyUSB0; sleep 0.1; done;
+while true; do echo "\$GTPOS,12931,Drone,1,2,3" > /dev/ttyUSB0; sleep 0.1; done;
